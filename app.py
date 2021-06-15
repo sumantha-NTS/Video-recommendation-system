d7c3b2bfc90801fb534fa098a3cc0caf8e83e585
SUMANTHA.N.T.S <sumanthneechadi@gmail.com>
2021-06-15 17:04:38 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import pandas as pd
 
 app = Flask(__name__)
 
-cos_model = pickle.load(open('df.csv','rb'))
-dataset = pickle.load(open('dataset.csv','rb'))
+cos_model = pickle.load(open('cosine.csv','rb'))
+dataset = pickle.load(open('clean_dataset.csv','rb'))
 
 @app.route('/')
 def welcome():
@@ -19,10 +19,11 @@ def about():
 def results():
     search = request.form['search']
     x = [];z=pd.DataFrame(columns=dataset.columns)
-    for i in range(0, len(dataset)):
-        if dataset.Title[i].lower().find(search.lower()) >= 0:
-            x.append(dataset.iloc[i, :])
     try:
+        for i in range(0, len(dataset)):
+            if dataset.Title[i].lower().find(search.lower()) >= 0:
+                x.append(dataset.iloc[i, :])
+    
         sel_df = pd.DataFrame(x).sort_values('rating', ascending=False)
         res = cos_model.iloc[:, sel_df.index[0]].sort_values(ascending=False).head(10).index
         for i in res:
